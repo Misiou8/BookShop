cfce6e9ffc5590da331fe8857f563ffa94515e28
Misiou8 <michal.szewczyk@microsoft.wsei.edu.pl>
2020-01-30 19:43:02 +0100

    Added tests

---
+++ BookShop/BookShopTest/Models/BookTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BookShop.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace BookshopTest
+{
+    [TestClass]
+    public class BookTests
+    {
+        private string _name, _genre;
+        private int _year;
+
+        [TestInitialize]
+        public void InitializeTests()
+        {
+            _name = "Higurashi";
+            _genre = "Horror";
+            _year = 2015;
+        }
+
+        [TestMethod]
+        public void AllBookData_isValid()
+        {
+            var book = new Book()
+            {
+                Name = _name,
+                Genre = _genre,
+                Year = _year
+            };
+
+            var context = new ValidationContext(book);
+            var result = new List<ValidationResult>();
+
+            var valid = Validator.TryValidateObject(book, context, result, true);
+
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod]
+        public void NameGenre_notWithCapitalLetter()
+        {
+            var book = new Book()
+            {
+                Name = "costam",
+                Genre = "jeszczejak",
+                Year = 1900
+            };
+
+            var result = TestModelHelper.Validate(book);
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void NameGenre_LengthIsLessThanExpected()
+        {
+            var book = new Book()
+            {
+                Name = "Co",
+                Genre = "Si",
+            };
+
+            var result = TestModelHelper.Validate(book);
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+
+        public void Year_TooFew()
+        {
+            var book = new Book()
+            {
+                Name = "Elo",
+                Genre = "Ziomeczki",
+                Year = 1899
+            };
+
+            var result = TestModelHelper.Validate(book);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void Year_NotFuture()
+        {
+            var book = new Book()
+            {
+                Name = "Swietna",
+                Genre = "Futurystyczna",
+                Year = 2022
+            };
+
+            var result = TestModelHelper.Validate(book);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void Year_CannotPutString()
+        {
+            var book = new Book()
+            {
+                Name = new string('Z', 41),
+                Genre = new string('X', 41),
+                Year = _year
+            };
+
+            var result = TestModelHelper.Validate(book);
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestCleanup]
+        public void CleanupTests()
+        {
+            _name = null;
+            _genre = null;
+            _year = 0;
+        }
+    }
+}
---
+++ BookShop/BookShopTest/Models/PublisherTests.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using BookShop.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookshopTest
+{
+    [TestClass]
+    public class PublisherTests
+    {
+        private string _name, _street, _postalCode, _city, _phone;
+        private int _streetNumber;
+
+        [TestInitialize]
+        public void InitializeTests()
+        {
+            _name = "Waneko";
+            _street = "Orzeszkowa";
+            _streetNumber = 33;
+            _postalCode = "80-306";
+            _city = "Gdańsk";
+            _phone = "555 123 532";
+        }
+
+        [TestMethod]
+        public void AllPublisherData_AreValid()
+        {
+            var publisher = new Publisher()
+            {
+                Name = _name,
+                Street = _street,
+                StreetNumber = _streetNumber,
+                PostalCode = _postalCode,
+                City = _city,
+                Phone = _phone
+            };
+
+            var context = new ValidationContext(publisher);
+            var result = new List<ValidationResult>();
+
+            var valid = Validator.TryValidateObject(publisher, context, result, true);
+
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod]
+        public void NameStreetCity_TooShort()
+        {
+            var publisher = new Publisher()
+            {
+                Name = "O",
+                Street = "XDD",
+                StreetNumber = _streetNumber,
+                PostalCode = _postalCode,
+                City = "W",
+                Phone = _phone
+            };
+
+            var result = TestModelHelper.Validate(publisher);
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void NameStreetCity_TooLong()
+        {
+            var publisher = new Publisher()
+            {
+                Name = new string('X', 31),
+                Street = new string('D', 41),
+                StreetNumber = _streetNumber,
+                PostalCode = _postalCode,
+                City = new string('D', 31),
+                Phone = _phone
+            };
+
+            var result = TestModelHelper.Validate(publisher);
+            Assert.AreEqual(3, result.Count);
+        }
+        [TestMethod]
+        public void NameStreetCity_NotCapitalChar()
+        {
+            var publisher = new Publisher()
+            {
+                Name = "insignis",
+                Street = "złomowa",
+                StreetNumber = _streetNumber,
+                PostalCode = _postalCode,
+                City = "sosnowiec",
+                Phone = _phone
+            };
+
+            var result = TestModelHelper.Validate(publisher);
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void StreetNumber_Wrong()
+        {
+            var publisher = new Publisher()
+            {
+                Name = _name,
+                Street = _street,
+                StreetNumber = 0,
+                PostalCode = _postalCode,
+                City = _city,
+                Phone = _phone
+            };
+
+            var result = TestModelHelper.Validate(publisher);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void PostalCode_Wrong()
+        {
+            var publisher = new Publisher()
+            {
+                Name = _name,
+                Street = _street,
+                StreetNumber = _streetNumber,
+                PostalCode = "123-52",
+                City = _city,
+                Phone = _phone
+            };
+
+            var result = TestModelHelper.Validate(publisher);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void Phone_Wrong()
+        {
+            var publisher = new Publisher()
+            {
+                Name = _name,
+                Street = _street,
+                StreetNumber = _streetNumber,
+                PostalCode = _postalCode,
+                City = _city,
+                Phone = "12345656352"
+            };
+
+            var result = TestModelHelper.Validate(publisher);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _name = null;
+            _street = null;
+            _streetNumber = 0;
+            _postalCode = null;
+            _city = null;
+            _phone = null;
+        }
+    }
+}
